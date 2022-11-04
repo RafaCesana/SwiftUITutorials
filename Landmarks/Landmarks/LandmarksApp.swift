65b8309ea9b8be52808f7d030bd6f26045c9b68a
Rafael Cesana <63931668+RafaCesana@users.noreply.github.com>
2022-11-04 06:18:30 -0300

    created project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rafael Cesana on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
